cc51f6cd39b1681ff2cf62eccfd5c8279658977c
Carlos Arboleda <carlos_arboleda@epam.com>
2026-02-16 17:44:23 -0500

    feat: Display application version

--- src/components/GameView/MainHeader.tsx
+++ src/components/GameView/MainHeader.tsx
@@ -10,8 +10,9 @@ export const MainHeader = () => {
     <>
       <div className="border-b border-(--color-terminal-light)/30 bg-(--color-terminal-dark) px-4 py-2">
         <div className="flex flex-row w-full items-center justify-between">
-          <h1 className="mb-1 text-2xl font-bold text-white">
+          <h1 className="flex gap-2 mb-1 text-2xl font-bold text-white items-end">
             Babel's Signal Routing
+            <span className="text-sm text-gray-400">v{__APP_VERSION__}</span>
           </h1>
           <span className="text-sm text-gray-400">
             {completedLevels.length}/{levels.length} completed
