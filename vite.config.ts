35a5a2cef2d600fe7d1a599e1752a5cf17bd3787
Carlos Arboleda <carlos_arboleda@epam.com>
2026-02-21 17:22:44 -0500

    feat: integrate Google Tag Manager

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,18 @@ import { readFileSync } from "fs";
 const packageJson = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "html-transform",
+      transformIndexHtml(html) {
+        return html.replace(
+          /%VITE_GOOGLE_TAG_ID%/g,
+          process.env.VITE_GOOGLE_TAG_ID || "",
+        );
+      },
+    },
+  ],
   base: "/",
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
